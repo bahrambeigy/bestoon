b432508b78e99b3a9fbf16abc5a3d7f7b225954a
Bahram <bahramwhh@gmail.com>
2017-02-18 16:22:23 +0330

    Added some RESTful api :)

--- web/models.py
+++ web/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 
 
 # Create your models here
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
--- web/views.py
+++ web/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from web.models import User, Token, Expense, Income
+
+from django.views.decorators.csrf import csrf_exempt
+
+from datetime import datetime
 
 # Create your views here.
+
+@csrf_exempt
+def submit_income(request):
+    """user submits and expense """
+
+    #TODO valid might be fake
+
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' in request.POST:
+        date = request.POST['date']
+    else:
+        date = datetime.now()
+    Income.objects.create(user=this_user, amount=request.POST['amount'], text = request.POST['text'], date=date)
+
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)
+
+
+@csrf_exempt
+def submit_expense(request):
+    """user submits and expense """
+
+    #TODO valid might be fake
+
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' in request.POST:
+        date = request.POST['date']
+    else:
+        date = datetime.now()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'], text = request.POST['text'], date=date)
+
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)
